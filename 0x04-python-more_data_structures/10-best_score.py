0c4673d9a50896e754c41bec3473fea5422a8726
Gamaliel Adun <52507933+Gamwal@users.noreply.github.com>
2022-12-09 16:52:35 -0500

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,6 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
-        mx = max(a_dictionary.values())
         for x, y in a_dictionary.items():
-            if y == mx:
+            if y == max(a_dictionary.values():
                 return x
