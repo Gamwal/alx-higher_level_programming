3df23da1fa3764333fdc60a8b3b64de89b41b2fa
Gamaliel Adun <adungamaliel@gmail.com>
2022-12-20 21:44:02 +0000

    added 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 class Square():
-    """
-    This class object defines a square with no other parameters
-    """
+    """defines a square with no other parameters"""
     def __init__(self):
        pass
