c0ecaaaba6d04e3b7f088c9ba4ec72076a206fa6
Gamaliel Adun <adungamaliel@gmail.com>
2022-12-21 03:26:36 -0800

    added 1-sqaure.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a Square by size"""
+
+    def __init__(self, size):
+        """initializes Square object with size passed as an arg"""
+        self.__size = size
