b5825829d54bf08545ddd1451bb1330dc88e39ff
Gamaliel Adun <adungamaliel@gmail.com>
2022-12-21 23:02:16 +0000

    added 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,20 +4,19 @@
 
 class Square:
     """Create a class Square."""
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Initializes the class object.
 
         Args:
             size (:obj: int, optional): The size of the square
                 must be >= 0.
 
+            position (:obj: tuple, optional): The position of the
+                square.
+
         """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
+        self.__position = position
 
     def area(self):
         """Calculates and returns the current sqaure area."""
@@ -44,9 +43,26 @@ class Square:
 
         if self.__size == 0:
             print("")
+            return
         else:
+            [print("") for i in range(0, self.__position[1])]
             for i in range(0, self.__size):
-                for j in range(0, self.__size):
-                    print("{}".format("#"), end='')
-                if i != self.__size:
-                    print("")
+                [print(" ", end="") for j in range(0, self.__position[0])]
+                [print("#", end="") for k in range(0, self.__size)]
+                print("")
+
+    @property
+    def position(self):
+        """retrieves the values of the position attribute"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the position attribute value."""
+
+        if not isinstance(value, tuple) or len(value) != 2 or\
+                not all(isinstance(num, int) for num in value) or\
+                not all(num >= 0 for num in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
