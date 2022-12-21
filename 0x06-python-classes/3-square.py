8497b076e9eb190ef4c32d1b76133a2445e39213
Gamaliel Adun <adungamaliel@gmail.com>
2022-12-21 04:39:04 -0800

    added 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a Square Class object"""
+
+
+class Square:
+    """Create a class Square."""
+    def __init__(self, size=0):
+        """Initializes the class object.
+
+        Args:
+            size (:obj: int, optional): The size of the square
+                must be >= 0.
+
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculates and returns the current sqaure area."""
+        return self.__size ** 2
