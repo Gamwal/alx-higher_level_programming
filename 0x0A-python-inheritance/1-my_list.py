41ffff54760c56fb4188c479192bbe6c19f55de0
Gamaliel Adun <52507933+Gamwal@users.noreply.github.com>
2023-01-09 16:38:57 -0500

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
+"""Defines a class MyList that inherits from list"""
 
 
 class MyList(list):
     """list object inheriting from python list."""
 
-    def __init__(self):
-        """initializes the object"""
-
-        list.__init__(self)
-
     def print_sorted(self):
         """method to print sorted list."""
-
         print(sorted(self))
