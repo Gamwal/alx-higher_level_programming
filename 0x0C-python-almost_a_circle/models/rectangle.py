a7f7860f5ac681095b9ba4056a55658b0d992694
Gamaliel Adun <adungamaliel@gmail.com>
2023-01-13 07:20:48 -0800

    added rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Rectangle(Base):
+
+    __width
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super.__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @setter
+    def __width(self, width):
+        
