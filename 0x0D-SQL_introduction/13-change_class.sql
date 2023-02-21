8e282d87f738bacf9879e518dee968983ad34d88
Gamaliel Adun <adungamaliel@gmail.com>
2023-02-21 12:34:34 -0800

    updated 13-change_class.sql

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,2 +1,2 @@
--- sql script to list all records with score >= 10 of second_table in hbtn_0c_0 DB
+-- sql script to delete all records with score <= 5 of second_table in hbtn_0c_0 DB
 DELETE FROM second_table where score <=5;
