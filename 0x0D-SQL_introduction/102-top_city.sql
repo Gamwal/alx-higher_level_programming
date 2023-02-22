c762b7af81a30cf22d495e5ae0ab0efa3c9d859b
Gamaliel Adun <adungamaliel@gmail.com>
2023-02-22 01:53:19 -0800

    issoryt

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- sql to display top 3 cities by temperature during July and August descending
+SELECT city, AVG(value) AS 'avg_temp' 
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
