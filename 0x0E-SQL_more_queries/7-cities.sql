3a6250ac2de90b3a8b58829b24da944094467b85
Gamaliel Adun <adungamaliel@gmail.com>
2023-02-23 12:39:04 -0800

    7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id int NOT NULL AUTO_INCREMENT,
 	state_id int NOT NULL
 	name varchar(256) NOT NULL
-	FOREIGN KEY (state_id) REFERENCES states(id)
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
 	);
