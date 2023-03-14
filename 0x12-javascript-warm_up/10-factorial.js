de70dae7a41b6e8b5dca58ea30cd28c715859061
Gamaliel Adun <adungamaliel@gmail.com>
2023-03-14 13:19:33 +0000

    updated 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,9 +3,9 @@ function factorial (a) {
   if (isNaN(a)) {
     return (1);
   } else if (a === 1) {
-      return (1);
+    return (1);
   } else {
-      return (a * factorial(a - 1));
+    return (a * factorial(a - 1));
   }
 }
 console.log(factorial(process.argv[2]));
