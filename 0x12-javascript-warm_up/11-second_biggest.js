66cad7fd8525f583de4fc84818e1d920401d6ebf
Gamaliel Adun <adungamaliel@gmail.com>
2023-03-14 14:49:58 +0000

    updated 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const arr = process.argv.slice(2).map(Number);
-const sortedArr = arr.sort(function(a, b) {
+const sortedArr = arr.sort(function (a, b) {
   return a - b;
 });
 if (arr.length === 0) {
