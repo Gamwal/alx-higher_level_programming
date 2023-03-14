9577d6920c331dbec12048078b6a924d1abc3359
Gamaliel Adun <adungamaliel@gmail.com>
2023-03-14 19:28:37 +0000

    updated 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (width, height) {
+    if (width > 0 &&
+      height > 0 &&
+      Number.isInteger(width) &&
+      Number.isInteger(height)) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
