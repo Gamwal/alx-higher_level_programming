148282ed9d0e590689e1662c164df6d396e7038f
Gamaliel Adun <adungamaliel@gmail.com>
2023-04-06 14:02:58 +0000

    updated 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,4 +6,5 @@ def find_peak(lst):
     """funtion that takes a list of integers and finds the peak"""
     if len(lst) == 0:
         return None
-    return max(lst)
+    slst = lst.sort()
+    return lst[-1]
