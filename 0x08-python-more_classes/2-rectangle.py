89e4ce233f5bd135d244b222d78097b132392d1f
Gamaliel Adun <adungamaliel@gmail.com>
2023-04-14 08:45:19 +0000

    updated 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" This is the definition of a class object Rectangle"""
+
+
+class Rectangle:
+    """Simple Rectangle Class"""
+    def __init__(self, width=0, height=0):
+        """init method of the Rectangle class"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter function for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter function for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """getter function for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter function for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """area function of the Rectangle object"""
+        return self.width * self.height
+    
+    def perimeter(self):
+        """area function of the Rectangle object"""
+        return (2 * self.width) + (2 * self.height)
