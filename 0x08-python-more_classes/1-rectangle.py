63aac760239d9e3c2f74e45eb9dee0681d4f1d91
Gamaliel Adun <adungamaliel@gmail.com>
2023-04-14 08:40:27 +0000

    updated 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Simple Rectangle Class"""
     def __init__(self, width=0, height=0):
         """init method of the Rectangle class"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -19,11 +19,11 @@ class Rectangle:
         """setter function for width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
-
+ 
     @property
     def height(self):
         """getter function for height"""
@@ -34,7 +34,7 @@ class Rectangle:
         """setter function for height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
