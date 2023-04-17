c276052b8038b35eb56c81ecdf0d1f043b89718f
Gamaliel Adun <52507933+Gamwal@users.noreply.github.com>
2023-04-17 15:58:58 +0100

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -7,4 +7,4 @@ class BaseGeometry:
 
     def area(self):
         """area method that does nothing yet"""
-        raise Exception "area() is not implemented"
+        raise Exception("area() is not implemented")
