17a8051ce1b7debe2050d5e54bc06399aece84e7
Gamaliel Adun <adungamaliel@gmail.com>
2023-04-17 08:05:58 -0700

    updated 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Geometry Module with area method"""
+
+
+class BaseGeometry:
+    """Empty base class with an area method"""
+
+    def area(self):
+        """area method that does nothing yet"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """public instance method that validated value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
