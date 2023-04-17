6db55192983bd40ccd4c650c2457b29a1d248854
Gamaliel Adun <adungamaliel@gmail.com>
2023-04-17 08:52:48 -0700

    updated 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Rectangle module that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Init method for the BaseGeometry class"""
+        self.__width = BaseGeometry.integer_validator(self, "width", width)
+        self.__height = BaseGeometry.integer_validator(self, "height", height)
+
+    def area(self):
+        """Implemented area method of the rectangle class object"""
+        return self.width * self.height
+
+    def __str__(self):
+        """string magic method"""
+        return "{}/{}".format(self.width, self.height)
