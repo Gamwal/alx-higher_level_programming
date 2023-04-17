eea9aa13f1f3d5e98e84042d9bad68e03d8cd8a5
Gamaliel Adun <adungamaliel@gmail.com>
2023-04-17 07:57:57 -0700

    updated 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Geometry Module"""
+"""Geometry Module with area method"""
 
 
 class BaseGeometry:
-    """Empty base class"""
+    """Empty base class with an area method"""
+
     def area(self):
+        """area method that does nothing yet"""
         raise Exception "area() is not implemented"
