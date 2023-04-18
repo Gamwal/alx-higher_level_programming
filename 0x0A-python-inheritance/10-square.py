3feac8352468fb0bd9c6c533df07b3d5af4b28b0
Gamaliel Adun <adungamaliel@gmail.com>
2023-04-18 01:17:11 -0700

    updated 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square module that inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square object that inherits from Rectangle"""
+    def __init__(self, size):
+        """initmethod of the Square object that takes in size"""
+        self.__size = BaseGeometry.integer_validator(self, "size", size)
