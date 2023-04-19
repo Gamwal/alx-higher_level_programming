1c7255960a0c26d83918b5931a1dfc49776368b3
Gamaliel Adun <adungamaliel@gmail.com>
2023-04-19 03:43:29 -0700

    updated 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list, and then save them to a file"""
+"""
+This module is a script that adds all arguments to a Python list,
+and then saves them to a file
+"""
 
 
 import sys
