6db8e4486519508d36e09420ecefcdd04365e12e
Gamaliel Adun <52507933+Gamwal@users.noreply.github.com>
2023-04-19 11:50:17 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,7 @@ import os
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__== "__main__":
+if __name__ == "__main__":
     if not os.path.exists('add_item.json'):
         with open('add_item.json', 'w', encoding='utf-8'):
             pass
