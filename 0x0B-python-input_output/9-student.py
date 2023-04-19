6d6d153fbca9f4e79183b8b6031d19f34008b392
Gamaliel Adun <52507933+Gamwal@users.noreply.github.com>
2023-04-19 13:28:51 +0100

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -15,12 +15,12 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def __dict__(self):
         return dict({'first_name': self.first_name,
                      'last_name': self.last_name,
                      'age': self.age})
-    
+
     def to_json(self):
         """Public method to retrieve a dict representation of object"""
-        return self.__dict__
+        return self.__dict__()
