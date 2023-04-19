e6c15a5e4a3b56081109b3edb046bc6350c300d7
Gamaliel Adun <adungamaliel@gmail.com>
2023-04-19 06:57:50 -0700

    updated 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -16,6 +16,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
+    @property
     def __dict__(self):
         return {'first_name': self.first_name,
                      'last_name': self.last_name,
@@ -24,18 +25,20 @@ class Student:
     def to_json(self, attrs=None):
         """Public method to retrieve a dict representation of object"""
         if attrs is None:
-            return self.__dict__()
+            return self.__dict__
         elif attrs is not None and type(attrs) == list:
             for attr in attrs:
                 if type(attr) != str:
-                    return self.__dict__()
+                    return self.__dict__
         new_dict = dict()
-        for key, value in self.__dict__():
+        for key, value in self.__dict__.items():
             if key in attrs:
                 new_dict[key] = value
         return new_dict
 
     def reload_from_json(self, json):
         """Public method to replace all attributes of student"""
+        for key, value in self.__dict__.items():
+            delattr(self, key)
         for key, value in json.items():
             setattr(self, key, value)
