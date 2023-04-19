e2cd043fed2ed834727b7c21215c88fe9be4b00f
Gamaliel Adun <52507933+Gamwal@users.noreply.github.com>
2023-04-19 13:03:26 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 function that returns the dictionary description with simple
-data structure (list, dictionary, string, integer and boolean) 
+data structure (list, dictionary, string, integer and boolean)
 for JSON serialization of an object
 """
 import json
@@ -9,5 +9,5 @@ import json
 
 def class_to_json(obj):
     """class to json function"""
-    my_obj =  json.dumps(obj.__dict__())
+    my_obj = json.dumps(obj.__dict__())
     return json.loads(my_obj)
