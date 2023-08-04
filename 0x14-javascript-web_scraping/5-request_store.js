71a3ae07d7934c9134727ad1787d963696d2b88a
Gamaliel Adun <adungamaliel@gmail.com>
2023-08-04 10:52:56 +0000

    updated 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -13,13 +13,13 @@ request.get(options, (err, response, body) => {
   if (err) {
     console.error(err);
   } else {
-    if (response.statusCode === 200 && body !== '') {
+    if (response.statusCode === 200 && body) {
       fs.writeFile(filePath, body, (err) => {
         if (err) {
           console.error(err);
         }
       });
-    } else if (response.statusCode === 200 && body === '') {
+    } else if (response.statusCode === 200 && !body) {
       fs.writeFile(filePath, '', (err) => {
         if (err) {
           console.error(err);
