07cfc5ed1b3b9cd9f9424f56230ed2928f3aa7a3
Gamaliel Adun <adungamaliel@gmail.com>
2023-08-04 11:17:34 +0000

    added incomplete 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const options = {
+  url: url,
+  json: true
+};
+
+request.get(options, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    if (response.statusCode === 200) {
+      const completedTasksByUser = {};
+      body.forEach((todo) => {
+        if (todo.completed) {
+          if (completedTasksByUser[todo.userId]) {
+            completedTasksByUser[todo.userId]++;
+          } else {
+            completedTasksByUser[todo.userId] = 1;
+          }
+        });
+      }
+    }
+  });
+console.log('Users with completed tasks:');
+  for (const userId in completedTasksByUser) {
+    console.log(`User ID: ${userId}, Completed Tasks: ${completedTasksByUser[userId]}`);
+  }
+} else {
+  console.error('Failed to fetch data. Status code:', response.statusCode);
+}
+}
+});
